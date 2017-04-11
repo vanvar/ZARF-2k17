06579821c43612e59a49340b7a1b2df5a4861028
Hashir Ahmad <hashir.ahmad7@gmail.com>
2017-04-11 17:12:27 +0530

    Added CollapsableCardView to Developers.java

--- app/src/main/java/zarf2k17/cs2k18/zarf2k17/Developers.java
+++ app/src/main/java/zarf2k17/cs2k18/zarf2k17/Developers.java
@@ -1,14 +1,75 @@
 package zarf2k17.cs2k18.zarf2k17;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class Developers extends AppCompatActivity {
+
+    private ViewGroup linearLayoutDetails;
+    private ImageView imageViewExpand;
+
+    private static final int DURATION = 250;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_developers);
-        getSupportActionBar().setHomeButtonEnabled(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getActionBar().setHomeButtonEnabled(true);
+
+        linearLayoutDetails = (ViewGroup) findViewById(R.id.linearLayoutDetails);
+        imageViewExpand = (ImageView) findViewById(R.id.imageViewExpand);
+
+        Toolbar toolbarCard = (Toolbar) findViewById(R.id.toolbarCard);
+        toolbarCard.setTitle(R.string.dev);
+        toolbarCard.setSubtitle(R.string.subtitle);
+        //toolbarCard.inflateMenu(R.menu.card_menu);
+        /*toolbarCard.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_option1:
+                        Toast.makeText(Developers.this, R.string.option1, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_option2:
+                        Toast.makeText(Developers.this, R.string.option2, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_option3:
+                        Toast.makeText(Developers.this, R.string.option3, Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });*/
     }
+
+    public void toggleDetails(View view) {
+        if (linearLayoutDetails.getVisibility() == View.GONE) {
+            ExpandAndCollapseView.expand(linearLayoutDetails, DURATION);
+            imageViewExpand.setImageResource(R.mipmap.more);
+            rotate(-180.0f);
+        } else {
+            ExpandAndCollapseView.collapse(linearLayoutDetails, DURATION);
+            imageViewExpand.setImageResource(R.mipmap.less);
+            rotate(180.0f);
+        }
+    }
+
+    private void rotate(float angle) {
+        Animation animation = new RotateAnimation(0.0f, angle, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setFillAfter(true);
+        animation.setDuration(DURATION);
+        imageViewExpand.startAnimation(animation);
+    }
+
+
 }
